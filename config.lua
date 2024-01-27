9f9f419fc5da1653438ece49598d365235abd9e5
RivickyS <90830337+rivickys@users.noreply.github.com>
2024-01-27 21:02:29 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,17 +2,13 @@ Config = {}
 
 -- Front Desk
 Config.Label = 'Call Officer' -- Label on qb-target
-Config.UsePed = false -- if true ped will spawn on the vector4 coords, or false using circle zone
+Config.UsePed = true -- if true ped will spawn on the vector4 coords, or false using circle zone
 
-Config.Target = {
-    Icon = 'fas fa-sign-in-alt',
-    coords = vector3(441.82, -992.44, 30.72), --Target Location
-    DebugPoly = false, -- Debug Target Zone
-}
+Config.coords = vector3(-341.89, -1487.74, 30.76) --Target Location
+Config.DebugPoly = true -- Debug Target Zone
 
-Config.Ped = {
-    model = 's_m_y_dwservice_02', -- Ped Model
-    coords = vector4(426.36, -980.97, 30.71, 81.55),  -- Ped Location
-    animDict = 'abigail_mcs_1_concat-0',
-    anim = 'csb_abigail_dual-0',
-}
+Config.Pedmodel = 's_m_y_dwservice_02' -- Ped Model
+Config.Pedcoords = vector4(-342.11, -1483.43, 30.7, 266.77)  -- Ped Location
+Config.PedanimDict = 'abigail_mcs_1_concat-0'
+Config.Pedanim = 'csb_abigail_dual-0'
+Config.Pedscenario = 'WORLD_HUMAN_STAND_MOBILE'
