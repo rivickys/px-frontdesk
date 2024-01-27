1ca486102aa8e05d88f18f08ec508ff4a75ddd19
RivickyS <90830337+rivickys@users.noreply.github.com>
2024-01-27 21:01:46 +0700

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,55 +1,52 @@
 CreateThread(function()
     if Config.UsePed then
-        for k, v in pairs(Config.Ped) do
-            exports["qb-target"]:SpawnPed({
-                model = v.model,
-                coords = v.coords,
-                minusOne = true,
-                freeze = true,
-                invincible = true,
-                blockevents = true,
-                animDict = v.animDict,
-                anim = v.anim,
-                flag = 1,
-                scenario = 'WORLD_HUMAN_STAND_MOBILE',
-                target = {
-                    options = {
-                        {
-                            type = "client",
-                            event = "px-frontdesk:dispatch",
-                            icon = "fas fa-sign-in-alt",
-                            label = Config.Label,
-                        }
-                    },
-                    distance = 2.5,
-                },
-                spawnNow = true,
-                currentpednumber = 0,
-            })
-        end
-    else
-        for k, v in pairs(Config.Target) do
-            exports['qb-target']:AddCircleZone("FrontDesk", v.coords, 1.5, {
-                name = "frontdeskalert",
-                debugPoly = v.DebugPoly,
-                }, {
+        exports["qb-target"]:SpawnPed({
+            model = Config.Pedmodel,
+            coords = Config.Pedcoords,
+            minusOne = true,
+            freeze = true,
+            invincible = true,
+            blockevents = true,
+            animDict = Config.PedanimDict,
+            anim = Config.Pedanim,
+            flag = 1,
+            scenario = Config.Pedscenario,
+            target = {
                 options = {
                     {
-                    num = 1,
-                    type = "client",
-                    event = "px-frontdesk:dispatch",
-                    icon = v.Icon,
-                    label = Config.Label,
-                    targeticon = 'fa-solid fa-eye',
+                        type = "client",
+                        event = "px-frontdesk:dispatch",
+                        icon = "fas fa-sign-in-alt",
+                        label = Config.Label,
                     }
                 },
-                distance = 2.5,
-            })
-        end
+                distance = 1.5,
+            },
+            spawnNow = true,
+            currentpednumber = 0,
+        })
+    else
+        exports['qb-target']:AddCircleZone("FrontDesk", Config.coords, 1.0, {
+            name = "frontdeskalert",
+            debugPoly = Config.DebugPoly,
+            }, {
+            options = {
+                {
+                num = 1,
+                type = "client",
+                event = "px-frontdesk:dispatch",
+                icon = 'fas fa-sign-in-alt',
+                label = Config.Label,
+                targeticon = 'fa-solid fa-eye',
+                }
+            },
+            distance = 2.5,
+        })
     end
 end)
 
 RegisterNetEvent('px-frontdesk:dispatch')
 AddEventHandler('px-frontdesk:dispatch', function()
+    print('1')
     exports['ps-dispatch']:FrontDesk('FrontDesk')
 end)
